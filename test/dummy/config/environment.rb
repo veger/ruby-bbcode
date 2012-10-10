93828ea1f0a6e973b705e9c6f30b87b3fed17326
Maarten Bezemer <maarten.bezemer@gmail.com>
2012-10-10 20:33:28 +0200

    Added missing configuration settings to fix running 'rake test'

--- test/dummy/config/environment.rb
+++ test/dummy/config/environment.rb
@@ -1,5 +1,15 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+
+Dummy::Application.configure do
+  # Log error messages when you accidentally call methods on nil
+  config.whiny_nils = true
+
+  # Print deprecation notices to the stderr
+  config.active_support.deprecation = :stderr
+end
+           
+
 # Initialize the rails application
 Dummy::Application.initialize!
