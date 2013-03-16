caf9910133c4fcabba997e56d57b9b01125ff48d
TheNotary <no@mail.plz>
2013-03-16 16:07:59 -0500

    tidy'd comments

--- lib/ruby-bbcode/tag_node.rb
+++ lib/ruby-bbcode/tag_node.rb
@@ -1,9 +1,9 @@
 module RubyBBCode
   # Tag nodes are nodes that are stored up in the BBTree's @current_node[:nodes] array I think... which is a bit misleading... 
   # hey, maybe that can be factored out into it's own instance variable 
-  # They specify either opening tag elements or text elements...  But that's what TagInfos do... It's just that TagInfo elements... are just discarded...
   #
-  # Pass in a hash...  It acts as a hash... but has a single cool method...
+  # They specify either opening tag elements or text elements...  TagInfo elements are essentially converted into these nodes which are
+  # later converted into html output in the bbtree_to_html method
   class TagNode
     attr_accessor :manifestation
     def initialize(element, nodes = [])
@@ -20,19 +20,21 @@ module RubyBBCode
     end
     
     def []=(key, value)
-      #binding.pry
       @element[key] = value
     end
     
-    # ... this is incomplete.....
+    # Checks to see if the parameter for the TagNode has been set.  
     def param_not_set?
       (@element[:params].nil? or @element[:params][:tag_param].nil?)
     end
     
+    # shows the tag definition for this TagNode as defined in tags.rb
     def definition
       @element[:definition]
     end
     
+    # Easy way to set the tag_param value of the hash, which represents 
+    # the parameter supplied
     def tag_param=(param)
       @element[:params] = {:tag_param => param}
     end
