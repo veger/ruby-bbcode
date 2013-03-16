ed503199dc4243fa7e1a4c322a217b5445e0fdd7
TheNotary <no@mail.plz>
2013-03-15 19:23:01 -0500

    realized the need for to add a new instance variable for the final tree that is being built up.  I just made comments.

--- lib/ruby-bbcode/tag_node.rb
+++ lib/ruby-bbcode/tag_node.rb
@@ -1,11 +1,14 @@
 module RubyBBCode
-  # Tag nodes are nodes that are placed in the BBTree collection.  
+  # Tag nodes are nodes that are stored up in the BBTree's @current_node[:nodes] array I think... which is a bit misleading... 
+  # hey, maybe that can be factored out into it's own instance variable 
   # They specify either opening tag elements or text elements...  But that's what TagInfos do... It's just that TagInfo elements... are just discarded...
-  
+  #
   # Pass in a hash...  It acts as a hash... but has a single cool method...
   class TagNode
-    def initialize(element)
+    attr_accessor :manifestation
+    def initialize(element, nodes = [])
       @element = element    # { :is_tag=>false, :text=>"ITALLICS" } ||   { :is_tag=>true, :tag=>:i, :nodes => [] }
+      @manifestation = nodes
       #@nodes = []  # TagNodes can contain other TagNodes as they rest in the BBTree
       # @current_node...  TagNodes shouldn't have @current_nodes... That's something only the BBTree has.  But
       # sometimes TagNodes get set directly to the BBTree I fear... this behavior must be changed before this 
