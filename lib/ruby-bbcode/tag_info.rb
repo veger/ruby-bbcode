1f28dc23623c1d61c6d454ed84240d26457c7996
TheNotary <no@mail.plz>
2013-03-16 16:20:12 -0500

    tidy'd up tag_info.rb

--- lib/ruby-bbcode/tag_info.rb
+++ lib/ruby-bbcode/tag_info.rb
@@ -1,7 +1,9 @@
 module RubyBBCode
-  # tag info is basically what the regex scan get's converted into during the tag_collection#process_text method
-  # 
-  # FIXME:  This files needs to be cleaned up... I skipped it.  
+  # tag info is basically what the regex scan get's converted into 
+  # during the tag_collection#process_text method.
+  # This class was made mostly just to keep track of all of the confusing
+  # the logic conditions that are checked.  
+  #
   class TagInfo
     def initialize(tag_info, dictionary)
       @tag_data = find_tag_info(tag_info)
@@ -17,21 +19,38 @@ module RubyBBCode
       @tag_data[key] = value
     end
     
+    def tag_data
+      @tag_data
+    end
+    
+    def definition
+      @definition
+    end
+    
+    # This represents the text value of the element (if it's not a tag element)
+    # Newlines are converted to html <br /> syntax before being returned.  
+    def text
+      text = @tag_data[:text]
+      # convert_newlines_to_br  
+      text.gsub!("\r\n", "\n")
+      text.gsub!("\n", "<br />\n")
+      text
+    end
+    
+    # allows for a very snazy case/ when conditional
+    def type
+      return :opening_tag if element_is_opening_tag?
+      return :text if element_is_text?
+      return :closing_tag if element_is_closing_tag?
+    end
+    
     def handle_unregistered_tags_as_text
-      if element_is_tag? and tag_missing_from_tag_list?
+      if element_is_tag? and tag_missing_from_tag_dictionary?
         # Handle as text from now on!
         self[:is_tag] = false
         self[:closing_tag] = false
         self[:text] = self[:complete_match]
       end
-    end
-    
-    def allowed_outside_parent_tags?
-      @definition[:only_in].nil?
-    end
-    
-    def constrained_to_within_parent_tags?
-      !@definition[:only_in].nil?
     end
     
     def element_is_tag?
@@ -50,17 +69,37 @@ module RubyBBCode
       !self[:text].nil?
     end
     
-    # allows for a very snazy case/ when conditional
-    def type
-      return :opening_tag if element_is_opening_tag?
-      return :text if element_is_text?
-      return :closing_tag if element_is_closing_tag?
+    def has_params?
+      self[:params][:tag_param] != nil
     end
-
-    def tag_missing_from_tag_list?
+    
+    def tag_missing_from_tag_dictionary?
       !@dictionary.include?(self[:tag].to_sym)
     end
+    
+    def allowed_outside_parent_tags?
+      @definition[:only_in].nil?
+    end
+    
+    def constrained_to_within_parent_tags?
+      !@definition[:only_in].nil?
+    end
+    
+    def allowed_in(tag_symbol)
+      @definition[:only_in].include?(tag_symbol)
+    end
+    
+    def can_have_params?
+      @definition[:allow_tag_param]
+    end
 
+    # Checks if the tag param matches the regex pattern defined in tags.rb
+    def invalid_param?
+      self[:params][:tag_param].match(@definition[:tag_param]).nil?
+    end
+    
+    protected
+    
     def find_tag_info(tag_info)
       ti = {}
       ti[:complete_match] = tag_info[0]
@@ -79,40 +118,6 @@ module RubyBBCode
         ti[:text] = tag_info[8]
       end
       ti
-    end
-    
-    def tag_data
-      @tag_data
-    end
-    
-    def definition
-      @definition
-    end
-    
-    def allowed_in(tag_symbol)
-      @definition[:only_in].include?(tag_symbol)
-    end
-    
-    def can_have_params?
-      @definition[:allow_tag_param]
-    end
-    
-    def has_params?
-      @tag_data[:params][:tag_param] != nil
-    end
-    
-    def invalid_param?
-      @tag_data[:params][:tag_param].match(@definition[:tag_param]).nil?
-    end
-    
-    # This represents the text value of the element (if it's not a tag element)
-    # Newlines are converted to html <br /> syntax before being returned.  
-    def text
-      text = @tag_data[:text]
-      # convert_newlines_to_br  
-      text.gsub!("\r\n", "\n")
-      text.gsub!("\n", "<br />\n")
-      text
     end
     
   end
