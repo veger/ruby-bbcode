c352854723b7aa5a37acb05f934b992690b25a02
TheNotary <no@mail.plz>
2013-04-01 18:27:00 -0500

    cleaned up comments

--- lib/ruby-bbcode/bbtree.rb
+++ lib/ruby-bbcode/bbtree.rb
@@ -10,18 +10,13 @@ module RubyBBCode
   # The closing of the nodes seems to be implied which is fine by me --less to keep track of.  
   # 
   class BBTree
-    include RubyBBCode::DebugBBTree   # this is for debugging the class.  Check lib/debugging.rb
-    attr_accessor :current_node, :tags_list, :manifestation
+    attr_accessor :current_node, :tags_list
     
     def initialize(hash = { :nodes => [] }, dictionary)
       @bbtree = hash
       @current_node = TagNode.new(@bbtree)
       @tags_list = []
       @dictionary = dictionary
-      
-      #@manifestation = TagNode.new({ :nodes => [] })
-      #binding.pry
-      @manifestation = TagNode.new({ :nodes => [] })
     end
     
     def [](key)
@@ -46,8 +41,6 @@ module RubyBBCode
       @dictionary[parent_tag][:only_allow] != nil
     end
     
-    
-    
     # Advance to next level (the node we just added)
     def escalate_bbtree(element)
       @tags_list.push element[:tag]
@@ -59,25 +52,20 @@ module RubyBBCode
       @tags_list.pop     # remove latest tag in tags_list since it's closed now... 
       # The parsed data manifests in @bbtree.current_node[:nodes] << TagNode.new(element) which I think is more confusing than needed
 
-      # Since we just stepped down we should set the current node to be the @bbtree...
-      # This works because the @bbtree includes everything except for the currently open node (which is being worked on)
-      # ...But where does the node get stored...  
-      #@current_node = TagNode.new(@bbtree) # Set current_node to be the whole @bbtree
-      
       if within_open_tag?
         # Set the current node to be the node we've just parsed over which is infact within another node??...
         @current_node = TagNode.new(@bbtree[:nodes].last)
       else # If we're still at the root of the BBTree or have returned back to the root via encountring closing tags...
         @current_node = TagNode.new(@bbtree)
       end
+      
+      # OKOKOK!  
+      # Since @bbtree = @current_node, if we ever set @current_node to something, we're actually changing @bbtree...
+      # therefore... my brain is now numb
     end
     
     def build_up_new_tag(element)
-      # @bbtree.current_node[:nodes] << TagNode.new(element)
-      #binding.pry
       @current_node[:nodes] << TagNode.new(element)
-      @manifestation << TagNode.new(element)    # Marshal.load Marshal.dump element
-      #@manifestation << TagNode.new(Marshal.load Marshal.dump element)
     end
     
   end
