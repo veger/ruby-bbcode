15cfc57b0569c5663d6be53cbafd2027753065b9
TheNotary <no@mail.plz>
2013-04-03 20:29:03 -0500

    ATTENTION:  refactored some code that I didn't exactly know what it's use cases were.  The method 'determine_applicable_tags' needs to be commented by code owner.

--- lib/ruby-bbcode.rb
+++ lib/ruby-bbcode.rb
@@ -11,39 +11,45 @@ require 'ruby-bbcode/bbtree'
 module RubyBBCode
   include ::RubyBBCode::Tags
 
+  # FIXME:  This needs documentation
   def self.to_html(text, escape_html = true, additional_tags = {}, method = :disable, *tags)
-    # We cannot convert to HTML if the BBCode is not valid!
     text = text.clone
-    use_tags = @@tags.merge(additional_tags)
-
-    if method == :disable then
-      tags.each { |t| use_tags.delete(t) }
-    else
-      new_use_tags = {}
-      tags.each { |t| new_use_tags[t] = use_tags[t] if use_tags.key?(t) }
-      use_tags = new_use_tags
-    end
-
+    
+    use_tags = determine_applicable_tags(additional_tags, method, *tags)
+    
+    # TODO:  Move this inside tag_sifter as an optional parameter upon initialization maybe?
     if escape_html
       text.gsub!('<', '&lt;')
       text.gsub!('>', '&gt;')
     end
 
     valid = parse(text, use_tags)
-    raise valid.join(', ') if valid != true
+    raise valid.join(', ') if valid != true   # We cannot convert to HTML if the BBCode is not valid!
     
     @tag_sifter.bbtree.to_html(use_tags)
   end
 
   def self.is_valid?(text, additional_tags = {})
-    parse(text, @@tags.merge(additional_tags));
+    parse(text, @@tags.merge(additional_tags));  
   end
   
   
   protected
-  def self.parse(text, tags = {})
-    tags = @@tags if tags == {}
-    
+  
+  # FIXME: this code needs to possibly have a better name and needs comments.  idk what it does...
+  def self.determine_applicable_tags(additional_tags, method, *tags)
+    use_tags = @@tags.merge(additional_tags)
+    if method == :disable then          # if user specified :disable
+      tags.each { |t| use_tags.delete(t) }   # disable tags supplied??
+    else  # user specified...????
+      new_use_tags = {}
+      tags.each { |t| new_use_tags[t] = use_tags[t] if use_tags.key?(t) }
+      use_tags = new_use_tags
+    end
+    use_tags
+  end
+  
+  def self.parse(text, tags)
     @tag_sifter = TagSifter.new(text, tags)
     
     @tag_sifter.process_text
