07f1f3c28a11d937770e32d2f1319fad66d5e20f
TheNotary <no@mail.plz>
2013-04-03 23:07:38 -0500

    fixed warning code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,8 +14,8 @@ require "test/unit"
 class Class
   def publicize_methods
     saved_private_instance_methods = self.private_instance_methods
-    self.class_eval { public *saved_private_instance_methods }
+    self.class_eval { public(*saved_private_instance_methods) }
     yield
-    self.class_eval { private *saved_private_instance_methods }
+    self.class_eval { private(*saved_private_instance_methods) }
   end
 end
