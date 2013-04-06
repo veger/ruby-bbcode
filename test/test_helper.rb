161be31cee4009daa49a2ec26238ee1f6fa0c606
Maarten Bezemer <maarten.bezemer@gmail.com>
2013-04-06 15:18:18 +0200

    Removed some rails references that are not required, as we do not use Rails anymore

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,7 @@
-# Configure Rails Environment
-#ENV["RAILS_ENV"] = "test"
-
-#require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-#require "rails/test_help"
-
-#Rails.backtrace_cleaner.remove_silencers!
-
 require 'ruby-bbcode'
 require "test/unit"
 
-
-#  This hack allows us to make all the private methods of a class public.  
+# This hack allows us to make all the private methods of a class public.  
 class Class
   def publicize_methods
     saved_private_instance_methods = self.private_instance_methods
