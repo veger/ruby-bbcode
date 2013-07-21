82a6be9f7c3b9f364c85af02bcc077d5af9d0642
TheNotary <no@mail.plz>
2013-07-21 13:28:41 -0500

    fixed security hole allowing xss

--- test/ruby_bbcode_test.rb
+++ test/ruby_bbcode_test.rb
@@ -216,5 +216,11 @@ class RubyBbcodeTest < Test::Unit::TestCase
       "this [b]should not do formatting[/i]".bbcode_to_html
     end
   end
+  
+  def test_no_xss_hax
+    require 'pry';binding.pry
+    expected = "<a href=\"http://www.google.com&quot; onclick=\&quot;javascript:alert\">google</a>"
+    assert_equal expected, '[url=http://www.google.com" onclick="javascript:alert]google[/url]'.bbcode_to_html
+  end
 
 end
