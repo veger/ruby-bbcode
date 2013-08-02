8a1d6242f0417872f03d46425eb1e08bf5721190
TheNotary <no@mail.plz>
2013-08-01 21:21:22 -0500

    trimmed out the superfluous video tags.

--- lib/tags/tags.rb
+++ lib/tags/tags.rb
@@ -96,38 +96,11 @@ module RubyBBCode
         :only_allow => [],
         :url_matches => [/vimeo\.com\/([^&]*)/],
         :require_between => true},
-      :veoh => {
-        :html_open => '<object width="410" height="341" id="veohFlashPlayer" name="veohFlashPlayer"><param name="movie" value="http://www.veoh.com/swf/webplayer/WebPlayer.swf?version=AFrontend.5.7.0.1404&permalinkId=%between%&player=videodetailsembedded&videoAutoPlay=0&id=anonymous"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.veoh.com/swf/webplayer/WebPlayer.swf?version=AFrontend.5.7.0.1404&permalinkId=%between%&player=videodetailsembedded&videoAutoPlay=0&id=anonymous" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="410" height="341" id="veohFlashPlayerEmbed" name="veohFlashPlayerEmbed"></embed></object><br />',
-        :html_close => '',
-        :description => 'moar videos, some full length ones',
-        :example => '[veoh]http://www.veoh.com/watch/v825695EXrZWRfH[/veoh]',
-        :only_allow => [],
-        :url_matches => [/veoh\.com\/watch\/([^&]*)/],
-        :require_between => true},
-      :flickr => {
-        :html_open => '<object type="application/x-shockwave-flash" width="400" height="300" data="http://www.flickr.com/apps/video/stewart.swf?v=109786" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"> <param name="flashvars" value="intl_lang=en-us&photo_secret=b4d35d51bd&photo_id=%between%"></param> <param name="movie" value="http://www.flickr.com/apps/video/stewart.swf?v=109786"></param> <param name="bgcolor" value="#000000"></param> <param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="http://www.flickr.com/apps/video/stewart.swf?v=109786" bgcolor="#000000" allowfullscreen="true" flashvars="intl_lang=en-us&photo_secret=b4d35d51bd&photo_id=%between%" height="300" width="400"></embed></object>',
-        :html_close => '',
-        :description => 'videos by fickr, a picture company',
-        :example => '[flickr]http://www.flickr.com/photos/antimega/2397432981[/flickr]',
-        :only_allow => [],
-        :url_matches => [/flickr\.com\/photos\/.*\/([^&]*)/],
-        :require_between => true},
-      :engage_media => {
-        :html_open => "<iframe src='http://www.engagemedia.org/Members/indocs/videos/%between%/embed_view' frameborder='0' width='630' height='460'></iframe>",
-        :html_close => '',
-        :description => 'Videos about earth and humans and stuff.  They distinctly have no cesorship which is rare.',
-        :example => '[engage_media]http://www.nbcnews.com/id/3032600#52211333[/engage_media]',
-        :only_allow => [],
-        :url_matches => [/engagemedia\.org\/Members\/.*\/videos\/([^&]*)\/view/],
-        :require_between => true},
       :media => {
         :multi_tag => true,
         :supported_tags => [
           :youtube,
-          :vimeo,
-          :flickr,
-          :veoh,
-          :engage_media
+          :vimeo
         ]
       }
     }
