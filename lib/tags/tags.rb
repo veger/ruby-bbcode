5de6e855abc4526ca44e2317abe9fc6668e68bc8
Ilya Levin <almaron@gmail.com>
2019-05-25 11:14:56 +0300

    Allow other tags inside url

--- lib/tags/tags.rb
+++ lib/tags/tags.rb
@@ -67,11 +67,9 @@ module RubyBBCode
                               { :token => :height,  :prefix => 'height="', :postfix => '" ', :optional => true } ],
         :quick_param_format_description => 'The image parameters \'%param%\' are incorrect, \'<width>x<height>\' excepted'},
       :url => {
-        :html_open => '<a href="%url%">%between%', :html_close => '</a>',
+        :html_open => '<a href="%url%">', :html_close => '</a>',
         :description => 'Link to another page',
         :example => '[url]http://www.google.com/[/url].',
-        :only_allow => [],
-        :require_between => true,
         :allow_quick_param => true, :allow_between_as_param => true,
         :quick_param_format => /^((((http|https|ftp):\/\/)|\/).+)$/,
         :quick_param_format_description => 'The URL should start with http:// https://, ftp:// or /, instead of \'%param%\'',
